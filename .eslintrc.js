75a7a9c290411fe8a8ce74254b256f4a53a2b39e
ventrova <wrayo39ryan@gmail.com>
2020-08-22 12:54:40 -0400

    'original'

---
+++ .eslintrc.js
@@ -0,0 +1,20 @@
+module.exports = {
+    "env": {
+        "browser": true
+    },
+    "extends": "eslint:recommended",
+    "rules": {
+        "indent": [
+            "error",
+            4
+        ],
+        "linebreak-style": [
+            "error",
+            "unix"
+        ],
+        "quotes": [
+            "error",
+            "double"
+        ]
+    }
+};
---
+++ script.js
@@ -0,0 +1,5 @@
+document.getElementById("btn").onclick= function() {
+    document.getElementById("btn").style.display="none";
+    document.getElementById("timer").style.display="block";
+    document.getElementById("timer").style.textAlign="center";
+}
